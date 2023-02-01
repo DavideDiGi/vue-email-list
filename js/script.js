a92bb9c8d1fe7006d01d79164e7a0daa37f05e92
Davide Di Girolamo <davide.digi2001@gmail.com>
2023-02-01 18:01:16 +0100

    Esercizio finito

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ createApp({
     data() {
 
         return {
-
+            emailAddress: [],
         };
     },
 
@@ -13,8 +13,19 @@ createApp({
 
     },
 
-    mounted() {
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(
 
+                (resp) => {
+                    console.log(resp);
+                    console.log(resp.data);
+
+                    this.emailAddress.push(resp.data.response);
+                }
+
+            );
+        }
     }
 
 }).mount('#app')
